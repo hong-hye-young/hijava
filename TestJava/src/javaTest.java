2a5e4454988c55de52bcac12cc5badc18ff493fe
hong project <hongproject2019@gmail.com>
2019-10-24 01:16:55 +0900

    브랜치 테스트

--- TestJava/src/javaTest.java
+++ TestJava/src/javaTest.java
@@ -4,6 +4,7 @@ public class javaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java!!");
+		System.out.println("java branch Test");
 	}
 
 }
