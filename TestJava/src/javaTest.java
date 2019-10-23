f6c711276380ad08df9eed15b7613bbca548a95f
hong project <hongproject2019@gmail.com>
2019-10-24 01:26:53 +0900

    브랜치 독립성 확인

--- TestJava/src/javaTest.java
+++ TestJava/src/javaTest.java
@@ -5,6 +5,7 @@ public class javaTest {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java!!");
 		System.out.println("java branch Test");
+		System.out.println("브랜치 독립성 확인");
 	}
 
 }
